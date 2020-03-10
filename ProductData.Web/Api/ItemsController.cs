f90f5c72026f6f6f6c6044e57cc4932128395b65
hthong <hthong.developer@gmail.com>
2020-03-10 00:33:39 -0700

    add endpoint to get item max price

--- ProductData.Web/Api/ItemsController.cs
+++ ProductData.Web/Api/ItemsController.cs
@@ -1,5 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Results;
 using ProductData.ApplicationServices.Entity;
 using ProductData.ApplicationServices.Interface;
 
@@ -15,16 +18,42 @@ namespace ProductData.Web.Api
         {
             _productDataServices = productDataServices;
         }
-     
-       
+
+
         [HttpGet]
         [Route("group/name/agg/max(price)")]
-        public IEnumerable<MaxPriceItemByName> GetMaxPriceItemByNames()
+        public IHttpActionResult GetMaxPriceItemByNames()
         {
-            return _productDataServices.GetMaxPricesByItemName();
+            try
+            {
+                var result = _productDataServices
+                    .GetMaxPricesByItemName();
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+                return InternalServerError(new Exception(e.Message));
+            }
         }
 
+        [HttpGet]
+        [Route("group/name/agg/max(price)")]
+        public IHttpActionResult GetMaxPriceItemByName(string name)
+        {
+            try
+            {
+                var result = _productDataServices
+                    .GetMaxPricesByItemName()
+                    .FirstOrDefault(q => q.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
 
+                return result != null ? (IHttpActionResult) Ok(result) : new NotFoundResult(this); 
+            }
+            catch (Exception e)
+            {
+                return InternalServerError(new Exception(e.Message));
+            }
+           
+        }
 
     }
 }
