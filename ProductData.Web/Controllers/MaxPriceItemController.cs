9c95d40a2f13679adef7257ee5e391887fa92b02
hthong <hthong.developer@gmail.com>
2020-03-10 03:11:06 -0700

    add UI

---
+++ ProductData.Web/Controllers/MaxPriceItemController.cs
@@ -0,0 +1,36 @@
+﻿using System.Web.Mvc;
+using ProductData.ApplicationServices.Interface;
+
+namespace ProductData.Web.Controllers
+{
+    public class MaxPriceItemController : Controller
+    {
+        private readonly IProductDataApiServices _services;
+
+        public MaxPriceItemController(IProductDataApiServices services)
+        {
+            _services = services;
+        }
+
+        // GET: Product
+        public ActionResult Index()
+        {
+            var model = _services.GetHighestCostItems();
+            return View(model);
+        }
+
+        public ActionResult Details(string name)
+        {
+
+            var model = _services.GetHighestCostItemByName(name);
+            return model == null
+                ? (ActionResult) RedirectToAction("NotFound")
+                : View(model);
+        }
+
+        public ActionResult NotFound()
+        {
+            return View();
+        }
+    }
+}
